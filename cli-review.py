8f07242b8965315a13548bb50927fcbe2e18d54e
Reece Stevens <reecestevens24@gmail.com>
2022-08-10 08:38:36 -0500

    Initial setup on a test branch

--- cli-review.py
+++ cli-review.py
@@ -1,16 +1,21 @@
 from argparse import ArgumentParser
+import os
 
 import github
 
+
 def get_args():
     parser = ArgumentParser()
-    parser.add_argument('--start-review')
-    parser.add_argument('branch')
+    parser.add_argument("--start-review")
+    parser.add_argument("branch")
     return parser.parse_args()
 
-def main(args):
-    pass
 
-if __name__ == '__main__':
+def main(args):
+    gh = github.Github(os.environ.get('GH_API_TOKEN'))
+    repo = gh.get_repo('reecestevens/cli-github-pr-review')
+
+
+if __name__ == "__main__":
     args = get_args()
     main(args)
