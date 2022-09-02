873476c360f896e50103dfc292b7b9f5c5399ac4
Reece Stevens <reecestevens24@gmail.com>
2022-09-02 18:20:45 -0500

    WIP: Basic neovim plugin functionality for starting review

--- rplugin/python3/nvim-plugin.py
+++ rplugin/python3/nvim-plugin.py
@@ -1,17 +1,30 @@
+import os
+from typing import Optional
+
 import pynvim
+import offline_pr_review
 
 
 @pynvim.plugin
 class TestPlugin(object):
     review_active: bool
+    review: Optional[offline_pr_review.Review]
 
     def __init__(self, nvim):
         self.review_active = False
         self.nvim = nvim
+        remote_info = self.nvim.call('FugitiveRemote')
+        offline_pr_review.update_configuration(remote_info['path'].replace('.git', ''))
 
-    @pynvim.command('StartReview')
-    def start_review(self):
+    @pynvim.command('StartReview', nargs=1)
+    def start_review(self, args):
+        self.review = offline_pr_review.new_blank_review(args[0])
         self.review_active = True
+
+    @pynvim.command('PublishReview')
+    def publish_review(self):
+        self.review_active = False
+        self.review.publish(os.getenv("GH_API_TOKEN"))
 
     @pynvim.function('IsReviewActive', sync=True)
     def is_review_active(self):
